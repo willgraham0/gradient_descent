03a4d5b337e9ac0633c2d4dab972686fa4d9f1e8
Will Graham <will.graham0@gmail.com>
2019-03-09 12:22:30 +0000

    feat: implement an execution module

---
+++ execute.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from descent import GradientDescent
+from functions import Normal
+
+
+def main():
+    mu, sig = 1.0, 3.0
+    x = np.linspace(-5.0, 5.0, 10)
+    y = Normal(mu, sig).f(x)
+
+    mu_guess, sig_guess = 0.1, 3.8
+    model = Normal(mu_guess, sig_guess)
+    descent = GradientDescent(model, x, y, iterations=11, learning_rate=50)
+
+    descent.run()
