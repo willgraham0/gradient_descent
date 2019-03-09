d625af46d24f93cf22ea0950631305931cc0dc51
Will Graham <will.graham0@gmail.com>
2019-03-09 12:21:32 +0000

    feat: implement the run method

--- descent.py
+++ descent.py
@@ -5,8 +5,13 @@ import numpy as np
 
 class GradientDescent:
 
-    def __init__(self, function, x: np.array, y: np.array, iterations: int = 100, learning_rate: int = 50):
-        """"""
+    def __init__(
+            self,
+            function,
+            x: np.array,
+            y: np.array,
+            iterations: int = 100,
+            learning_rate: int = 50):
         self.function = function
         self.x = x
         self.y = y
@@ -14,17 +19,22 @@ class GradientDescent:
         self.learning_rate = learning_rate
 
     def run(self):
+        """Perform the gradient descent."""
         for i in range(self.iterations):
-            print(f'Step={i} Cost={self.cost()} Variables={vars(self.function)}')
-            new_vars = old_vars * self.jacobian() * self.learning_rate
+            print(f'Step={i:{len(str(self.iterations))}} Cost={self.cost:.5E} Variables={vars(self.function)}')
+            new_variables = self.function.variables - self.jacobian * self.learning_rate
+            self.function.set_variables(new_variables)
 
+    @property
     def cost(self) -> float:
-        """Return mod(y - f(x; mu, sig))**2."""
-        return (self.y - self.function.f(self.x)).dot(self.y - self.function.f(self.x))
+        """Return mod(y - f(x; variables))**2."""
+        difference_vector = self.y - self.function.f(self.x)
+        return difference_vector.dot(difference_vector)
 
-    def jacobian(self):
-        """"""
-        return [
-            -2*(self.y - self.function.f(self.x)).dot(partial_derivative)
-            for partial_derivative in self.function.first_partial_derivatives()
-        ]
+    @property
+    def jacobian(self) -> np.array:
+        """Return the Jacobian (vector of first derivative of each variable)."""
+        return np.array([
+            -2 * (self.y - self.function.f(self.x)).dot(partial_derivative(self.x))
+            for partial_derivative in self.function.get_partial_derivatives()
+        ])
