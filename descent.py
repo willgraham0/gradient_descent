38fe52cd10674e3f87f20e4c9276181a5836a341
Will Graham <will.graham0@gmail.com>
2019-03-18 18:33:54 +0000

    feat: create plot contours method for GradientDescent class

--- descent.py
+++ descent.py
@@ -1,18 +1,17 @@
 """This module provides the gradient descent functionality."""
 
+from collections import OrderedDict
+from itertools import product
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 class GradientDescent:
 
-    def __init__(
-            self,
-            function,
-            x: np.array,
-            y: np.array,
-            iterations: int = 100,
-            learning_rate: int = 50):
+    def __init__(self, function, x: np.array, y: np.array, iterations: int = 100, learning_rate: int = 50):
         self.function = function
+        self.function_type = type(function)
         self.x = x
         self.y = y
         self.iterations = iterations
@@ -22,7 +21,7 @@ class GradientDescent:
     def run(self):
         """Perform the gradient descent."""
         for i in range(self.iterations + 1):
-            self.results.append({'step': i, 'cost': self.cost, 'variables': dict(vars(self.function))})
+            self.results.append({'step': i, 'cost': self.cost, 'variables': self.function.variables})
 
             # Increment function variables in the direction of largest cost reduction
             new_variables = self.function.get_variables - self.jacobian * self.learning_rate
@@ -42,17 +41,13 @@ class GradientDescent:
             for partial_derivative in self.function.get_partial_derivatives()
         ])
 
-    def print_results(self, cost_precision=5, variables_precision=2):
+    def print_results(self, cost_precision: int = 5, variables_precision: int = 2):
         """Print the results of the gradient descent."""
         step_name, cost_name, variables_name = 'Step', 'Cost', 'Variables'
         digits = len(str(self.iterations))
         buffer = len(step_name) if len(step_name) > digits else digits
 
-        print(
-            f'{step_name:{buffer}} | '
-            f'{cost_name:{cost_precision + 6}} | '
-            f'{variables_name}'
-        )
+        print(f'{step_name:{buffer}} | {cost_name:{cost_precision + 6}} | {variables_name}')
 
         for row in self.results:
             variables = ' '.join([
@@ -60,3 +55,64 @@ class GradientDescent:
                 for name, value in row['variables'].items()
             ])
             print(f"{row['step']:<{buffer}} | {row['cost']:.{cost_precision}E} | {variables}")
+
+    def plot_cost_contours(self, steps: int = 100, vectors=True):
+        """Plot a 2-dimensional contour plot of cost versus the two function variables.
+
+        The contours will be in the region of the minimum and maximum values of the function variables
+        calculated during the gradient descent.
+        """
+        if len(self.function.get_variables) > 2:
+            raise NotImplementedError("Cannot plot 2-dimensional plot with more than 2 variables.")
+        if not self.results:
+            raise ValueError('The gradient descent must be executed first.')
+
+        if vectors:
+            raise NotImplementedError()
+
+        variable_ranges = self._get_variable_ranges(steps=steps)
+
+        costs = np.array([
+            GradientDescent(self.function_type(*variables), self.x, self.y).cost
+            for variables in product(*variable_ranges.values())
+        ])
+
+        costs = costs.reshape(*map(len, variable_ranges.values()))
+        costs = costs.T
+        var1, var2 = np.meshgrid(*variable_ranges.values())
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        cs = ax.contour(var1, var2, costs, levels=np.linspace(0, 0.01, 10))
+        var1_label, var2_label = variable_ranges.keys()
+        plt.xlabel(var1_label)
+        plt.ylabel(var2_label)
+        plt.clabel(cs, inline=1, fontsize=10)
+        plt.title(f'Contours of cost with {var1_label} and {var2_label}')
+        plt.show()
+
+    def _get_variable_values(self) -> OrderedDict:
+        """Return all the values calculated for each variable.
+
+        E.g.
+        OrderedDict({
+            'var1': [1.2, 1.1, 0.8, 0.3],
+            'var2': [2.3, 3.1, 3.5, 3.5],
+        })
+        """
+        return OrderedDict(
+            {name: [result['variables'][name] for result in self.results] for name in self.function.variables.keys()}
+        )
+
+    def _get_variable_ranges(self, steps: int = 100) -> OrderedDict:
+        """Return arrays of values for each variable between the minimum and maximum.
+
+        E.g.
+        OrderedDict({
+            'var1': np.array([0.3, 0.4, ..., 1.2]),
+            'var2': np.array([2.3, 3.1, ..., 3.5]),
+        })
+        """
+        return OrderedDict(
+            {name: np.linspace(min(values), max(values), steps) for name, values in self._get_variable_values().items()}
+        )
