49a1314df6f566036c9444c28613008965ae07b7
Will Graham <will.graham0@gmail.com>
2019-03-26 09:26:19 +0000

    docs: add docstring to utils.diff

--- utils.py
+++ utils.py
@@ -2,5 +2,9 @@ from typing import List
 
 
 def diff(data: List) -> List:
-    """Return the element by element differences."""
+    """Return the element by element differences.
+
+    E.g.
+    diff([1, 2, 3, 5, 8, 13]) -> [1, 1, 2, 3, 5]
+    """
     return [j-i for i, j in zip(data[:-1], data[1:])]
